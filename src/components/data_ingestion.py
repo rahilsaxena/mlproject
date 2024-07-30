fb90e3c7ffe9a143f924442fb30d8a4968d38f24
Rahil Saxena <rahilsaxena111@gmail.com>
2024-07-30 20:18:34 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,7 +6,9 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+ 
 
 @dataclass
 class DataIngestionConfig:
